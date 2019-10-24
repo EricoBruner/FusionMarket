ead68602cd1591334f5b8e50aee5702a829d8544
cedup <cedup@RYLuser>
2019-10-23 21:43:04 -0300

    Começo do cadastro do Usuário (view)

--- src/FusionMarket/FusionMarket.java
+++ src/FusionMarket/FusionMarket.java
@@ -2,10 +2,6 @@ public class FusionMarket
 {
     public static void main(String[] args) 
     {
-        String nome;
-        
-        double numero;
-        String nomecacau;
-        String hugo;
+      
     }
 }
