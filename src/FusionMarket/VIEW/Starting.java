754d74837f89df90ee4803fadc364ce7032c7348
EricoBruner <ericodasilvabruner@gmail.com>
2019-10-31 16:41:52 -0300

    Arrumando

--- src/FusionMarket/VIEW/Starting.java
+++ src/FusionMarket/VIEW/Starting.java
@@ -6,7 +6,7 @@ public class Starting
 {
     public static void main(String[] args) 
     {
-        TelaPrincipal telaprincipal  = new TelaPrincipal();
+        telaPrincipal telaprincipal  = new telaPrincipal();
         telaprincipal.setVisible(true);
         telaprincipal.setExtendedState(Frame.MAXIMIZED_BOTH);
     }
