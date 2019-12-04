68a09cf50c60cf709d22dd5a10069e9d46ae87f2
EricoBruner <ericodasilvabruner@gmail.com>
2019-12-04 21:53:18 -0200

    arrumei tela

--- src/FusionMarket/DAO/Usuario_DAO.java
+++ src/FusionMarket/DAO/Usuario_DAO.java
@@ -23,27 +23,33 @@ public class Usuario_DAO
     public Usuario_POJO checkLogin(String login, String senha)  
     {   
         Connection con= cn.getConnection();
-            PreparedStatement stmt=null;
-            ResultSet rs=null;
-            boolean check = false;
-            Usuario_POJO up=new Usuario_POJO();
-            try {
-               stmt=con.prepareStatement("SELECT * FROM usuario where nome=? and senha=?");
-               stmt.setString(1, login);
-               stmt.setString(2, senha);
+        
+        PreparedStatement stmt=null;
+        ResultSet rs=null;
+        boolean check = false;
+        Usuario_POJO up=new Usuario_POJO();
+        try
+        {
+            stmt=con.prepareStatement("SELECT * FROM usuario where nome=? and senha=?");
+            stmt.setString(1, login);
+            stmt.setString(2, senha);
                
-               rs=stmt.executeQuery();
+            rs=stmt.executeQuery();
                
-               while(rs.next()){
-               
-                   up.setId(rs.getInt("id_usuario"));
-                   //fazer todos
+            while(rs.next())
+               {
+                up.setId(rs.getInt("id_usuario"));
+                up.setNome(rs.getString("nome"));
+                up.setEmail(rs.getString("email"));
+                up.setEndereco(rs.getString("endereco"));
+                up.setEndereco(""+rs.getInt("cpf_cnpj"));
                }
-            }   catch (SQLException ex) {
-                    //Logger.getLogger(Usuario_DAO.class.getName()).log(Level.SEVERE, null, ex);
-            } 
-          return up;  
         }
-    
-    }
+        catch (SQLException ex) 
+        {
+
+        } 
+        return up;  
+    } 
+}
     
